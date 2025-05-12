3fbd9114b5aea188cfed42950b012a206bdce82c
Ben Muthalaly <benmuthalaly@gmail.com>
2025-05-12 17:09:09 -0400

    Open window with raylib.

--- build.zig
+++ build.zig
@@ -4,6 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = std.Build.standardTargetOptions(b, .{});
     const optimize = std.Build.standardOptimizeOption(b, .{});
 
+    // defining dependencies and modules
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
     const main_module = b.addModule("main", .{
         .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "src/main.zig" } },
         .target = target,
@@ -14,6 +24,10 @@ pub fn build(b: *std.Build) void {
         .root_module = main_module,
         .name = "floating_point",
     });
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
+const rl = @import("raylib");
+
+const window_width = 800;
+const window_height = 600;
 
 pub fn main() void {
-    std.debug.print("Hello world!\n", .{});
+    rl.initWindow(window_width, window_height, "floating point");
+
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
+        rl.clearBackground(rl.Color.black);
+
+        rl.beginDrawing();
+        defer rl.endDrawing();
+    }
+
+    defer rl.closeWindow();
 }
